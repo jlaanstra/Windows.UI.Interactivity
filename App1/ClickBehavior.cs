5d42d81060bc584b9869e9f82dd950e82c5d578b
Johan Laanstra <jlaanstra1221@hotmail.com>
2012-06-29 22:32:41 +0200

    Initial checkin including sample.

---
+++ App1/ClickBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Interactivity;
+
+namespace App1
+{
+    public class ClickBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(ClickBehavior), new PropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get { return (ICommand)this.GetValue(ClickBehavior.CommandProperty); }
+            set { this.SetValue(ClickBehavior.CommandProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.Tapped += AssociatedObject_Tapped;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.Tapped -= AssociatedObject_Tapped;
+        }
+
+        void AssociatedObject_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            if (this.Command != null)
+            {
+                this.Command.Execute(null);
+            }
+        }
+    }
+}
---
+++ App1/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace App1
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public DelegateCommand(Func<object,bool> canExecute, Action<object> execute)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
