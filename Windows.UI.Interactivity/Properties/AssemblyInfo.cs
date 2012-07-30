5fed2ae7791c484e48130edd71897697f7ae1e96
Johan Laanstra <jlaanstra1221@hotmail.com>
2012-07-30 15:27:01 +0200

    Updated version number to 1.0.1.0
    Cleaned up .gitignore

--- Windows.UI.Interactivity/Properties/AssemblyInfo.cs
+++ Windows.UI.Interactivity/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Windows.UI.Interactivity")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows.UI.Interactivity is a port of System.Windows.Interactivity to the Windows Runtime and allows you to use behaviors and triggers as in Silverlight.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Johan Laanstra")]
 [assembly: AssemblyProduct("Windows.UI.Interactivity")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: ComVisible(false)]
