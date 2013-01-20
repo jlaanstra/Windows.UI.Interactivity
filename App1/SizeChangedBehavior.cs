2b3a0b0ac5c48dfea79dc7aa21dddb46259a9e04
Johan Laanstra <jlaanstra1221@hotmail.com>
2013-01-20 15:03:03 +0100

    Added signing key.

---
+++ App1/SizeChangedBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Interactivity;
+using Windows.UI.Xaml;
+
+namespace App1
+{
+    class SizeChangedBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.SizeChanged += AssociatedObject_SizeChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            this.AssociatedObject.SizeChanged -= AssociatedObject_SizeChanged;
+        }
+
+        void AssociatedObject_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Debug.WriteLine("!!");
+        }
+    }
+}
